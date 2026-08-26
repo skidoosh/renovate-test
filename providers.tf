76fa97512ae831f8e08840866aa3e44757f691ec
Glyn Mooney <749058+skidoosh@users.noreply.github.com>
2026-08-26 11:12:25 +0100

    Test 36

--- providers.tf
+++ providers.tf
@@ -7,10 +7,41 @@ terraform {
     }
 }
 
-variable "vpc_version" {
-  description = "The version of the VPC module to use"
-  type        = string
-  default     = "5.0.0"
+module "kms" {
+  source = "terraform-aws-modules/kms/aws"
+  version = "3.0.0"
+
+  description         = "External key example"
+  key_material_base64 = "Wblj06fduthWggmsT0cLVoIMOkeLbc2kVfMud77i/JY="
+  valid_to            = "2085-04-12T23:20:50.52Z"
+
+  # Policy
+  key_owners         = ["arn:aws:iam::012345678901:role/owner"]
+  key_administrators = ["arn:aws:iam::012345678901:role/admin"]
+  key_users          = ["arn:aws:iam::012345678901:role/user"]
+  key_service_users  = ["arn:aws:iam::012345678901:role/ec2-role"]
+
+  # Aliases
+  aliases                 = ["mycompany/external"]
+  aliases_use_name_prefix = true
+
+  # Grants
+  grants = {
+    lambda = {
+      grantee_principal = "arn:aws:iam::012345678901:role/lambda-function"
+      operations        = ["Encrypt", "Decrypt", "GenerateDataKey"]
+      constraints = {
+        encryption_context_equals = {
+          Department = "Finance"
+        }
+      }
+    }
+  }
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
 
 module "vpc" {
