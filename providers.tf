41304222968289826e9e9568b4462c1a6a0012a8
Glyn Mooney <749058+skidoosh@users.noreply.github.com>
2026-08-26 10:48:25 +0100

    Test 34

--- providers.tf
+++ providers.tf
@@ -6,3 +6,29 @@ terraform {
         }
     }
 }
+
+variable "vpc_version" {
+  description = "The version of the VPC module to use"
+  type        = string
+  default     = "5.0.0"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = var.vpc_version
+
+  name = "renovate"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
